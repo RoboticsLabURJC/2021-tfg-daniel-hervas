635a4aac8b75f3c0393217ff097f0ebeaf66fa6c
dhrodao <danielhervasrodao@gmail.com>
2021-06-20 16:29:32 +0200

    Added views and templates

--- researchs/django/mysite/polls/urls.py
+++ researchs/django/mysite/polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+# Espacio de nombres de la aplicación
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
--- researchs/django/mysite/polls/views.py
+++ researchs/django/mysite/polls/views.py
@@ -1,11 +1,21 @@
 from polls.models import Question
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 from .models import Question
 
 def index(req):
     recent_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in recent_questions])
-    return HttpResponse('Recent Questions: '+output)
+    data = {
+        'recent_questions':recent_questions,
+    }
+    return render(req, 'polls/index.html', data)
 
 def detail(req, question_id):
-    return HttpResponse('Looking at question: %s' % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+        data = {
+            'question':question,
+        }
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist.')
+    return render(req, 'polls/detail.html', data)
